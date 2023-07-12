df048d6311e86c827ddd20ebff00db22f60623d5
m.hor <m.hor@azki.com>
2023-07-12 16:38:20 +0330

    initialize project

---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-constants
+
+rules for constants
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-constants`:
+
+```sh
+npm install eslint-plugin-constants --save-dev
+```
+
+## Usage
+
+Add `constants` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "constants"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "constants/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/constant-name.js
@@ -0,0 +1,20 @@
+const rule = require("../../../lib/rules/constant-name");
+const { RuleTester } = require("eslint");
+
+const ruleTester = new RuleTester();
+ruleTester.run("no-underscore-var", rule, {
+    // Test cases that SHOULD NOT report any errors from the rule
+    valid: [{
+        code: "var foo = 5;",
+        filename : 'back-office/src/shared/constants/_refactoredFiles/branch/test/index.ts',
+        options: [{ rootDir: '/constants/_refactoredFiles' }]
+    }],
+    // Test cases that SHOULD report errors from the rule
+    invalid: [
+        {
+            code: "var branchTestX = 5;",
+            filename: "/work/apollo/apps/back-office/src/shared/constants/_refactoredFiles/branch/test/index.ts",
+            options: [{ rootDir: 'shared/constants/_refactoredFiles' }]
+        },
+    ],
+});

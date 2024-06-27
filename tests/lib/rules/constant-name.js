667d8017bafe1db17c272d2d212dfa00cd1b258d
Mobin Hor <m.hor@azki.com>
2024-06-27 13:34:26 +0330

    test: modify tests

--- tests/lib/rules/constant-name.js
+++ tests/lib/rules/constant-name.js
@@ -2,19 +2,20 @@ const rule = require("../../../lib/rules/constant-name");
 const { RuleTester } = require("eslint");
 
 const ruleTester = new RuleTester();
-ruleTester.run("no-underscore-var", rule, {
+ruleTester.run("path-depandant-constant-naming", rule, {
     // Test cases that SHOULD NOT report any errors from the rule
     valid: [{
-        code: "var foo = 5;",
-        filename : 'back-office/src/shared/constants/_refactoredFiles/branch/test/index.ts',
-        options: [{ rootDir: '/constants/_refactoredFiles' }]
+        code: "var branchTestX = 5;",
+        filename : '/constants/branch/test/index.ts',
+        options: [{ rootDir: 'shared/constants' }]
     }],
     // Test cases that SHOULD report errors from the rule
     invalid: [
         {
             code: "var branchTestX = 5;",
-            filename: "back-office/src/shared/constants/_refactoredFiles/branch/testTwo/index.ts",
-            options: [{ rootDir: 'shared/constants/_refactoredFiles' }]
+            filename: "/constants/branch/testTwo/index.ts",
+            options: [{ rootDir: 'shared/constants' }],
+            errors: ["branchTestTwoX would be fine."]
         },
     ],
 });
